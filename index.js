b308216a7e10862ceef32c05928fdfd190e60f64
X3rez <66400617+X3rez@users.noreply.github.com>
2021-01-23 09:47:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({limit:'50mb',extended:true}))
 
 
-app.get("/",(req,res)=>{
+app.get("/send",(req,res)=>{
   res.json({page:"principal"})
 })
 
